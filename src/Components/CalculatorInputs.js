b9bc7f0838a3f6b8182ef4bd7b8ed6827cb2ab56
joker69216 <hamaraad883@gmail.com>
2022-10-25 20:05:01 +0300

    add the design

--- src/Components/CalculatorInputs.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './CalculatorInputs.css';
-
-const CalculatorInputs = () => (
-  <div>
-    <input type="number" className="calcInput" />
-  </div>
-);
-export default CalculatorInputs;
