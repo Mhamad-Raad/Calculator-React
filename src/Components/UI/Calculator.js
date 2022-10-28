29eb071b96489eb0a9ec9fbbf9a9a127c57dcd47
joker69216 <hamaraad883@gmail.com>
2022-10-28 14:38:37 +0300

    change the components into a functional component

--- src/Components/UI/Calculator.js
+++ src/Components/UI/Calculator.js
@@ -1,109 +1,108 @@
-import React from 'react';
+import React, { useState } from 'react';
 import calculate from '../Logic/calculate';
 import './Calculator.css';
 
-class Calculator extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: '',
-      next: null,
-      operation: null,
-    };
-  }
+const Calculator = () => {
+  const [total, setTotal] = useState('');
+  const [next, setNext] = useState('');
+  const [operation, setOperation] = useState('');
 
-  addInput = (event) => {
+  const addInput = (event) => {
     const clickedNumber = event.target.innerText;
-    const { total, next, operation } = this.state;
     if (operation === null && next === null) {
       if (total === 0 || total === '0') {
-        this.setState({ total: '' });
+        setTotal('');
       }
       const temp = total.toString() + clickedNumber.toString();
-      this.setState({ total: temp });
+      setTotal(temp);
     } else if (operation !== null && total !== null) {
       if (next === null) {
-        this.setState({ next: clickedNumber.toString() });
+        next(clickedNumber.toString());
       } else {
         const temp = next.toString() + clickedNumber.toString();
-        this.setState({ next: temp });
+        setNext(temp);
       }
     }
   };
 
-  addOperation = (event) => {
+  const addOperation = (event) => {
     const clickedOperation = event.target.innerText;
-    this.setState({ operation: clickedOperation.toString() });
+    setOperation(clickedOperation.toString());
   };
 
-  callEqual = (event) => {
+  const callEqual = (event) => {
     const clickedEqual = event.target.innerText;
-    const result = calculate(this.state, clickedEqual);
-    this.setState(result);
+    const result = calculate({ total, next, operation },
+      clickedEqual);
+    setTotal(result.total);
+    setNext(result.next);
+    setOperation(result.operation);
   };
 
-  callAC = (event) => {
+  const callAC = (event) => {
     const clickedAC = event.target.innerText;
-    const result = calculate(this.state, clickedAC);
-    this.setState(result);
+    const result = calculate({ total, next, operation }, clickedAC);
+    setTotal(result.total);
+    setNext(result.next);
+    setOperation(result.operation);
   };
 
-  callReverse = (event) => {
+  const callReverse = (event) => {
     const clickedReverse = event.target.innerText;
-    const result = calculate(this.state, clickedReverse);
-    this.setState(result);
+    const result = calculate({ total, next, operation }, clickedReverse);
+    setTotal(result.total);
+    setNext(result.next);
+    setOperation(result.operation);
   };
 
-  callDot = (event) => {
+  const callDot = (event) => {
     const clickedReverse = event.target.innerText;
-    const result = calculate(this.state, clickedReverse);
-    this.setState(result);
+    const result = calculate({ total, next, operation }, clickedReverse);
+    setTotal(result.total);
+    setNext(result.next);
+    setOperation(result.operation);
   };
 
-  render = () => {
-    const { total, next, operation } = this.state;
+  return (
+    <div className="calculator">
+      <input type="text" readOnly value={operation === null ? total : next} className="calcInput" />
+      <div className="make-flex">
+        <div className="row">
+          <button type="button" onClick={callAC} className="calcButton">AC</button>
+          <button type="button" onClick={callReverse} className="calcButton">+/-</button>
+          <button type="button" onClick={addOperation} className="calcButton">%</button>
+          <button type="button" onClick={addOperation} className="calcButton orange">÷</button>
+        </div>
 
-    return (
-      <div className="calculator">
-        <input type="text" readOnly value={operation === null ? total : next} className="calcInput" />
-        <div className="make-flex">
-          <div className="row">
-            <button type="button" onClick={this.callAC} className="calcButton">AC</button>
-            <button type="button" onClick={this.callReverse} className="calcButton">+/-</button>
-            <button type="button" onClick={this.addOperation} className="calcButton">%</button>
-            <button type="button" onClick={this.addOperation} className="calcButton orange">÷</button>
-          </div>
+        <div className="row">
+          <button type="button" onClick={addInput} className="calcButton">7</button>
+          <button type="button" onClick={addInput} className="calcButton">8</button>
+          <button type="button" onClick={addInput} className="calcButton">9</button>
+          <button type="button" onClick={addOperation} className="calcButton orange">x</button>
+        </div>
 
-          <div className="row">
-            <button type="button" onClick={this.addInput} className="calcButton">7</button>
-            <button type="button" onClick={this.addInput} className="calcButton">8</button>
-            <button type="button" onClick={this.addInput} className="calcButton">9</button>
-            <button type="button" onClick={this.addOperation} className="calcButton orange">x</button>
-          </div>
+        <div className="row">
+          <button type="button" onClick={addInput} className="calcButton">4</button>
+          <button type="button" onClick={addInput} className="calcButton">5</button>
+          <button type="button" onClick={addInput} className="calcButton">6</button>
+          <button type="button" onClick={addOperation} className="calcButton orange">-</button>
+        </div>
 
-          <div className="row">
-            <button type="button" onClick={this.addInput} className="calcButton">4</button>
-            <button type="button" onClick={this.addInput} className="calcButton">5</button>
-            <button type="button" onClick={this.addInput} className="calcButton">6</button>
-            <button type="button" onClick={this.addOperation} className="calcButton orange">-</button>
-          </div>
+        <div className="row">
+          <button type="button" onClick={addInput} className="calcButton">1</button>
+          <button type="button" onClick={addInput} className="calcButton">2</button>
+          <button type="button" onClick={addInput} className="calcButton">3</button>
+          <button type="button" onClick={addOperation} className="calcButton orange">+</button>
+        </div>
 
-          <div className="row">
-            <button type="button" onClick={this.addInput} className="calcButton">1</button>
-            <button type="button" onClick={this.addInput} className="calcButton">2</button>
-            <button type="button" onClick={this.addInput} className="calcButton">3</button>
-            <button type="button" onClick={this.addOperation} className="calcButton orange">+</button>
-          </div>
-
-          <div className="row">
-            <button type="button" onClick={this.addInput} className="calcButton zero">0</button>
-            <button type="button" onClick={this.callDot} className="calcButton dot">.</button>
-            <button type="button" onClick={this.callEqual} className="calcButton orange equal">=</button>
-          </div>
+        <div className="row">
+          <button type="button" onClick={addInput} className="calcButton zero">0</button>
+          <button type="button" onClick={callDot} className="calcButton dot">.</button>
+          <button type="button" onClick={callEqual} className="calcButton orange equal">=</button>
         </div>
       </div>
-    );
-  };
-}
+    </div>
+  );
+};
 
 export default Calculator;
