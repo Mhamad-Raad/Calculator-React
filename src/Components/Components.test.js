38d3650f002740bb70942feece98213daabe2fe7
Alhajideen <salifunurudeen834@gmail.com>
2022-11-02 15:08:22 -0600

    add test for calculator and quotes components

--- src/Components/Components.test.js
+++ src/Components/Components.test.js
@@ -1,9 +1,19 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Home from '../Components/UI/Home';
+import Home from './UI/Home';
+import Quote from './UI/Quote';
+import ClacHome from './UI/ClacHome';
 
 describe('Testing Components', () => {
   test('Testing Homepage', () => {
     const home = render(<Home />);
     expect(home).toMatchSnapshot();
+  });
+  test('Testing Quotes', () => {
+    const quotes = render(<Quote />);
+    expect(quotes).toMatchSnapshot();
+  });
+  test('Testing Calculator', () => {
+    const calculator = render(<ClacHome />);
+    expect(calculator).toMatchSnapshot();
   });
 });
